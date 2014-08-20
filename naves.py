4b815ba765465d2a8fac1ef283d1d4986e7a9ef5
mateo <mateo.bosco@gmail.com>
2014-08-19 23:35:45 -0300

    agrego controlador, falta manejar el escape y el disparador

--- naves.py
+++ naves.py
@@ -3,7 +3,6 @@ import sys
 import random
 from pygame.locals import *
 
-
 ANCHO_PANTALLA = 640
 ALTO_PANTALLA = 480
 
@@ -28,7 +27,7 @@ class Nave(object):
 	def moverArriba(self):
 		if (self.posicion[1] + self.tamano[1] < 0):
 			return
-		self.posicion[1] += self.velocidad[1]
+		self.posicion[1] -= self.velocidad[1]
 
 	def moverAbajo(self):
 		if (self.posicion[1] - self.tamano[1] > ALTO_PANTALLA):
@@ -44,6 +43,7 @@ class Nave(object):
 
 	def __str__(self):
 		return "Jugador en posicion " + str(self.posicion)
+
 
 class Enemigo(object):
 	def __init__(self):
@@ -84,8 +84,7 @@ class ManejadorEnemigos(object):
 	def ciclo(self):
 		self.mover()
 		self.ciclos += 1
-		print(self.ciclos)
-		if (self.ciclos == 10):
+		if (self.ciclos == 60):
 			self.crearEnemigo()
 			self.ciclos = 0
 
@@ -94,16 +93,51 @@ class ManejadorEnemigos(object):
 			e.dibujar(pantalla)
 
 
+class Dibujador(object):
+
+	def __init__(self):
+		pygame.init()
+		self.pantalla = pygame.display.set_mode((ANCHO_PANTALLA, ALTO_PANTALLA))
+
+	def actualizar(self, dibujables):
+		self.pantalla.fill(pygame.Color(0,0,0))
+
+		for d in dibujables:
+			d.dibujar(self.pantalla)
+
+		pygame.display.flip()
+		pygame.time.wait(10)
+
+class Jugador(object):
+	def __init__(self):
+		self.nave = Nave()
+		self.vidas = 3
+
+	def dibujar(self,pantalla):
+		self.nave.dibujar(pantalla)
+
+
+class Controlador(object):
+	def __init__(self,jugador):
+		self.jugador = jugador
+
+	def aplicar(self):
+		keys = pygame.key.get_pressed()
+		if keys[K_LEFT]: self.jugador.nave.moverIzquierda()
+		elif keys[K_RIGHT]: self.jugador.nave.moverDerecha()
+		elif keys[K_DOWN]: self.jugador.nave.moverAbajo()
+		elif keys[K_UP]: self.jugador.nave.moverArriba()
+
 
 def main():
 	pygame.init()
-	pantalla = pygame.display.set_mode((ANCHO_PANTALLA, ALTO_PANTALLA))
-	clock = pygame.time.Clock()
+	dibujador = Dibujador()
 
-	jugador = Nave()
+	jugador = Jugador()
 	manejador = ManejadorEnemigos()
+	dibujables = [jugador,manejador]
 
-
+	controlador = Controlador(jugador)
 	while 1:
 
 		manejador.ciclo()
@@ -111,11 +145,9 @@ def main():
 			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
-		pantalla.fill(pygame.Color(0,0,0))
-		jugador.dibujar(pantalla)
-		manejador.dibujar(pantalla)
-		pygame.display.flip()
-		pygame.time.wait(10)
+		
+		dibujador.actualizar(dibujables)
+		controlador.aplicar()	
 
 
 
